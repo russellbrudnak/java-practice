c2c9379a56913719ece882f034669974874b2842
Russell Brudnak <russellbrudnak@gmail.com>
2022-10-24 17:31:34 -0700

    java

---
+++ java-intro/src/net/russellbrudnak/Main.java
@@ -0,0 +1,13 @@
+package net.russellbrudnak;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("minecraft java");
+        System.out.println("man");
+        System.out.println("at");
+        System.out.println("haz");
+        System.out.println("bedroc jem");
+    }
+}
